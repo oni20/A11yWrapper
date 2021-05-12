a1a97d0fd09174164e7c865bc25b068380b7e728
Oni20 <oniahmad20@gmail.com>
2021-05-11 20:11:07 -0400

    Added new Keyboard feature for Menu

--- js/components/Menu.js
+++ js/components/Menu.js
@@ -8,7 +8,7 @@ import Utility from './Utility';
 function A11yMenu(navContainer, options) {
   this.menuBox = null;
   this.mobileToggleNavBtn = null;
-  this.keys = {
+  this.keys = Object.freeze({
     tab: 9,
     enter: 13,
     esc: 27,
@@ -16,8 +16,10 @@ function A11yMenu(navContainer, options) {
     left: 37,
     up: 38,
     right: 39,
-    down: 40
-  };
+    down: 40,
+    end: 35,
+    home: 36,
+  });
   this.menuItemLinks = null;
   this.subMenuItemLinks = null;
 
@@ -43,6 +45,30 @@ function A11yMenu(navContainer, options) {
   }
 }
 
+// Create Sub menu
+A11yMenu.prototype.createSubMenu = function (subMenuArr, parentIndex) {
+  let context = this, subMenu = '<ul role="menu">';
+
+  subMenuArr.map((item, index) => {
+    let nestedSubMenu = '';
+
+    if (item.subMenu) {
+      nestedSubMenu = context.createSubMenu(item.subMenu, index);
+    }
+
+    let listItemClass = nestedSubMenu !== "" ? 'class="has-nestedmenu"' : '',
+      ariaAttrib = nestedSubMenu !== "" ? 'aria-expanded="false" aria-haspopup="true"' : '';
+
+    subMenu += `<li ${listItemClass}>
+                  <a role="menuitem" href="${item.link}" ${ariaAttrib} data-parentmenuindex=${parentIndex} data-submenuindex=${index}>${item.name}</a>
+                  ${nestedSubMenu}
+                </li>`;
+  });
+  subMenu += '</ul>';
+
+  return subMenu;
+}
+
 // Create Slider inside container
 A11yMenu.prototype.createMenu = function () {
   let context = this,
@@ -53,18 +79,13 @@ A11yMenu.prototype.createMenu = function () {
     let subMenu = ''
 
     if (menu.subMenu) {
-      subMenu += '<ul role="menu">';
-
-      menu.subMenu.map((item, index) => {
-        subMenu += `<li role="menuitem"><a href="${item.link}" data-parentmenuindex=${idx} data-submenuindex=${index}>${item.name}</a></li>`;
-      });
-      subMenu += '</ul>';
+      subMenu = context.createSubMenu(menu.subMenu, idx);
     }
 
     buildMenu += subMenu === ''
-      ? `<li role="menuitem"><a href="${menu.link}" data-menuindex=${idx}>${menu.name}</a></li>`
-      : `<li class="has-submenu" role="menuitem">
-          <a href="${menu.link}" data-menuindex=${idx} aria-expanded="false" aria-haspopup="true">${menu.name}</a>
+      ? `<li><a role="menuitem" href="${menu.link}" data-menuindex=${idx}>${menu.name}</a></li>`
+      : `<li class="has-submenu">
+          <a role="menuitem" href="${menu.link}" data-menuindex=${idx} aria-expanded="false" aria-haspopup="true">${menu.name}</a>
           ${subMenu}
         </li>`;
   });
@@ -84,7 +105,7 @@ A11yMenu.prototype.createMenu = function () {
       </nav>`
 
   context.navContainer.innerHTML = template;
-  context.menuItemLinks = document.querySelectorAll(`#menu_${context.menuID} >li>a`);  
+  context.menuItemLinks = document.querySelectorAll(`#menu_${context.menuID} >li>a`);
   context.subMenuItemLinks = document.querySelectorAll(`#menu_${context.menuID} >li li>a`);
 }
 
@@ -130,46 +151,72 @@ A11yMenu.prototype.hideSubMenu = function (element) {
   }
 };
 
+A11yMenu.prototype.isPrintableCharacter = function (str) {
+  return str.length === 1 && str.match(/\S/);
+};
+
+A11yMenu.prototype.handleClickEvent = function (isMainMenu, elem, className, event) {
+  let context = this,
+    subMenuLinks = elem.nextElementSibling ? elem.nextElementSibling.querySelectorAll('a') : null,
+    openedMenuList = elem.closest('nav').querySelectorAll('.open');
+
+  // Close all the menus at first except the current one
+  if (isMainMenu && openedMenuList) {
+    for (let i = 0; i < openedMenuList.length; i++) {
+      if (elem.parentNode !== openedMenuList[i]) {
+        openedMenuList[i].classList.remove('open');
+      }
+    }
+  }
+
+  if (context.Utility._hasClass(elem.parentNode, className)) {
+    let isSubMenuOpened = context.Utility._hasClass(elem.parentNode, 'open');
+    elem.parentNode.className = isSubMenuOpened ? className : `${className} open`;
+    elem.setAttribute('aria-expanded', isSubMenuOpened ? "false" : "true");
+  }
+
+  if (subMenuLinks) {
+    context.gotoSubIndex(subMenuLinks, 0);
+    event.preventDefault();
+  }
+};
+
 A11yMenu.prototype.eventHandlers = function () {
   let context = this;
 
   // Toggle button for mobile view
   context.mobileToggleNavBtn.addEventListener('click', (event) => {
     let toggleBtn = event.currentTarget,
-        menuHeight = context.menuBox.getBoundingClientRect().height;
+      menuHeight = context.menuBox.getBoundingClientRect().height;
 
     if (context.Utility._hasClass(toggleBtn, 'change')) {
-      toggleBtn.classList.remove('change');      
-      context.menuBox.closest('nav').style.removeProperty('height');
+      toggleBtn.classList.remove('change');
+      context.menuBox.closest('nav').style.removeProperty('max-height');
     } else {
       toggleBtn.classList.add('change');
-      context.menuBox.closest('nav').style.height = `${(menuHeight/16) + 4.0625}em`;
+      context.menuBox.closest('nav').style.maxHeight = '200em';
+      //`${(menuHeight/16) + 4.0625}em`;
     }
   });
 
   Array.prototype.forEach.call(context.menuItemLinks, function (menuLink, i) {
     // Main menu link click event
     menuLink.addEventListener("click", function (event) {
-      let subMenuLinks = this.nextElementSibling ? this.nextElementSibling.querySelectorAll('a') : null;
-
-      if (context.Utility._hasClass(this.parentNode, 'has-submenu')) {
-        let isSubMenuOpened = context.Utility._hasClass(this.parentNode, 'open');
-        this.parentNode.className = isSubMenuOpened ? "has-submenu" : "has-submenu open";
-        this.setAttribute('aria-expanded', isSubMenuOpened ? "false" : "true");        
-      }
-
-      if (subMenuLinks) {
-        context.gotoSubIndex(subMenuLinks, 0);
-      }
-
-      event.preventDefault();
+      context.handleClickEvent(true, this, 'has-submenu', event);
       return false;
     });
 
     // Main menu link keypress event
     menuLink.addEventListener("keydown", function (event) {
-      let prevdef = false,
-        subMenuLinks = this.nextElementSibling ? this.nextElementSibling.querySelectorAll('a') : null;
+      let prevdef = false, subMenuLinks = [];
+
+      if (this.nextElementSibling) {
+        for (let i = 0; i < this.nextElementSibling.children.length; i++) {
+          subMenuLinks.push(this.nextElementSibling.children[i].children[0]);
+        }
+      } else {
+        subMenuLinks = null
+      }
 
       switch (event.keyCode) {
         case context.keys.right:
@@ -179,6 +226,16 @@ A11yMenu.prototype.eventHandlers = function () {
 
         case context.keys.left:
           context.gotoIndex(parseInt(this.dataset.menuindex) - 1);
+          prevdef = true;
+          break;
+
+        case context.keys.home:
+          context.gotoIndex(0);
+          prevdef = true;
+          break;
+
+        case context.keys.end:
+          context.gotoIndex(context.menuItemLinks.length - 1);
           prevdef = true;
           break;
 
@@ -197,9 +254,16 @@ A11yMenu.prototype.eventHandlers = function () {
           prevdef = true;
           break;
 
-        case context.keys.esc:          
+        case context.keys.esc:
           this.closest('nav').querySelector('a').focus();
           prevdef = true;
+          break;
+
+        default:
+          if (context.isPrintableCharacter(event.key)) {
+            this.menu.setFocusByFirstCharacter(this, event.key);
+            flag = true;
+          }
           break;
       }
 
@@ -211,30 +275,85 @@ A11yMenu.prototype.eventHandlers = function () {
 
   Array.prototype.forEach.call(context.subMenuItemLinks, function (subMenuLink, i) {
 
+    subMenuLink.addEventListener("click", function (event) {
+      context.handleClickEvent(false, this, 'has-nestedmenu', event);
+      return false;
+    });
+
     // Sub menu link keypress event
     subMenuLink.addEventListener("keydown", function (event) {
-      let prevdef = true, siblings = this.closest('ul').querySelectorAll('a');
+      let prevdef = true, siblings = [];
+
+      for (let i = 0; i < this.closest('ul').children.length; i++) {
+        siblings.push(this.closest('ul').children[i].children[0]);
+      }
 
       switch (event.keyCode) {
+        case context.keys.enter:
+        case context.keys.space:
+          this.click();
+          prevdef = true;
+          break;
+
         case context.keys.right:
-          context.gotoIndex(parseInt(this.dataset.parentmenuindex) + 1);
-          context.hideSubMenu(this.closest('li.has-submenu').querySelector('a'));
-          context.menuItemLinks[parseInt(this.dataset.parentmenuindex) + 1].click();
+          if (context.Utility._hasClass(this.parentElement, 'has-nestedmenu')) {
+            this.click();
+            prevdef = true;
+
+          } else {
+            let parentMenuAnchor = this.closest('li.has-submenu').querySelector('a'),
+              nextMenuIndex = parseInt(parentMenuAnchor.dataset.menuindex) + 1;
+
+            context.gotoIndex(nextMenuIndex);
+            context.hideSubMenu(parentMenuAnchor);
+            context.menuItemLinks[nextMenuIndex].click();
+          }
           break;
 
         case context.keys.left:
-          context.gotoIndex(parseInt(this.dataset.parentmenuindex) - 1);
-          context.hideSubMenu(this.closest('li.has-submenu').querySelector('a'));
-          context.menuItemLinks[parseInt(this.dataset.parentmenuindex) - 1].click();
+          let parentSubMenuItem = this.closest('ul').closest('li.has-nestedmenu');
+
+          if (parentSubMenuItem) {
+            parentSubMenuItem.querySelector('a').click();
+            parentSubMenuItem.querySelector('a').focus();
+            prevdef = true;
+
+          } else {
+            let parentMenuAnchor = this.closest('li.has-submenu').querySelector('a'),
+              prevMenuIndex = parseInt(parentMenuAnchor.dataset.menuindex) - 1;
+
+            context.gotoIndex(prevMenuIndex);
+            context.hideSubMenu(parentMenuAnchor);
+            context.menuItemLinks[prevMenuIndex].click();
+          }
+
           break;
 
         case context.keys.tab:
           if (event.shiftKey) {
-            context.gotoIndex(parseInt(this.dataset.parentmenuindex) - 1);
+            if (this.closest('li').previousElementSibling) {
+              this.closest('li').previousElementSibling.querySelector('a').focus();
+            } else {
+              if (this.closest('li.has-nestedmenu')) {
+                let parentAnchorElement = this.closest('li.has-nestedmenu').querySelector('a');
+                parentAnchorElement.click();
+                parentAnchorElement.focus();
+              } else {
+                let parentAnchorElement = this.closest('li.has-submenu').querySelector('a');
+                context.gotoIndex(parseInt(parentAnchorElement.dataset.menuindex) - 1);
+                context.hideSubMenu(parentAnchorElement);
+              }
+            }
           } else {
-            context.gotoIndex(parseInt(this.dataset.parentmenuindex) + 1);
+            if (this.closest('li').nextElementSibling) {
+              this.closest('li').nextElementSibling.querySelector('a').focus();
+            } else {
+              let parentAnchorElement = this.closest('li.has-submenu').querySelector('a');
+              context.gotoIndex(parseInt(parentAnchorElement.dataset.menuindex) + 1);
+              context.hideSubMenu(parentAnchorElement);
+            }
           }
-          context.hideSubMenu(this.closest('li.has-submenu').querySelector('a'));
+
           break;
 
         case context.keys.down:
@@ -245,10 +364,20 @@ A11yMenu.prototype.eventHandlers = function () {
           context.gotoSubIndex(siblings, parseInt(this.dataset.submenuindex) - 1);
           break;
 
+        case context.keys.home:
+          context.gotoSubIndex(siblings, 0);
+          break;
+
+        case context.keys.end:
+          context.gotoSubIndex(siblings, siblings.length - 1);
+          break;
+
         case context.keys.esc:
-          context.gotoIndex(this.dataset.parentmenuindex);
-          context.hideSubMenu(this.closest('li.has-submenu').querySelector('a'));
+          let parentAnchor = this.closest('ul').closest('li').querySelector('a');
+          parentAnchor.click();
+          parentAnchor.focus();
           prevdef = true;
+
           break;
       }
 
