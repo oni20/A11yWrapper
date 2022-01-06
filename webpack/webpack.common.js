4b7b7b16a327ef318d4b44ddf94552b16a62069a
Oni20 <oniahmad20@gmail.com>
2022-01-06 11:51:55 -0500

    Separated webpack

---
+++ webpack/webpack.common.js
@@ -0,0 +1,81 @@
+const webpack = require("webpack");
+const path = require("path");
+
+//Plugins
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+
+const config = {
+	entry: {
+		A11yWrapper: [
+			"./js/A11yWrapper.js",
+			"./scss/A11yWrapper.scss",
+		],
+		SourcePage: ["./scss/SourcePage.scss"]
+	},
+	output: {
+		path: path.resolve(__dirname, "../dist"),
+		filename: "js/[name].min.js"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					{
+						loader: "css-loader",
+						options: {
+							url: false
+						}
+					},
+					{
+						loader: "sass-loader",
+						options: {
+							sourceMap: true
+						}
+					}
+				]
+			},
+			{
+				test: /\.m?js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env'],
+						plugins: ['@babel/plugin-transform-runtime', '@babel/plugin-syntax-dynamic-import']
+					}
+				}
+			}
+		]
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: "css/[name].css",
+			ignoreOrder: false // Enable to remove warnings about conflicting order
+		})
+	],
+	optimization: {
+		minimizer: [
+			new OptimizeCSSAssetsPlugin({
+				cssProcessorPluginOptions: {
+					preset: [
+						"default",
+						{
+							discardComments: {
+								removeAll: true
+							}
+						}
+					]
+				}
+			}),
+			new UglifyJsPlugin({
+				test: /\.js(\?.*)?$/i,
+				exclude: /\/nodeModules/
+			})
+		]
+	}
+};
+module.exports = config;
---
+++ webpack/webpack.dev.js
@@ -0,0 +1,7 @@
+const { merge } = require("webpack-merge");
+const webpackCommon = require('./webpack.common.js');
+
+module.exports = merge(webpackCommon, {
+	devtool: 'inline-source-map',
+	mode: 'development'
+});
